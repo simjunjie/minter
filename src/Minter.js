5ed3042f7f3b7b39085a16e3fd0c1f11ef84dd85
jj <email>
2021-11-18 17:11:53 +0800

    first commit

---
+++ src/Minter.js
@@ -0,0 +1,101 @@
+import { useEffect, useState } from "react";
+import {
+  connectWallet,
+  getCurrentWalletConnected,
+  createCollectible,
+} from "./interact.js";
+
+const Minter = (props) => {
+  const [walletAddress, setWallet] = useState("");
+  const [status, setStatus] = useState("");
+
+  const [mintAmount, setMintAmount] = useState("");
+
+  useEffect(async () => {
+    const { address, status } = await getCurrentWalletConnected();
+
+    setWallet(address);
+    setStatus(status);
+
+    addWalletListener();
+  }, []);
+
+  function addWalletListener() {
+    if (window.ethereum) {
+      window.ethereum.on("accountsChanged", (accounts) => {
+        if (accounts.length > 0) {
+          setWallet(accounts[0]);
+          setStatus("👆🏽 Write a message in the text-field above.");
+        } else {
+          setWallet("");
+          setStatus("🦊 Connect to Metamask using the top right button.");
+        }
+      });
+    } else {
+      setStatus(
+        <p>
+          {" "}
+          🦊{" "}
+          <a target="_blank" href={`https://metamask.io/download.html`}>
+            You must install Metamask, a virtual Ethereum wallet, in your
+            browser.
+          </a>
+        </p>
+      );
+    }
+  }
+
+  const connectWalletPressed = async () => {
+    const walletResponse = await connectWallet();
+    setStatus(walletResponse.status);
+    setWallet(walletResponse.address);
+  };
+
+  const onMintPressed = async () => {
+    const { success, status } = await publicMint(mintAmount);
+    setStatus(status);
+    if (success) {
+      setMintAmount("");
+    }
+  };
+
+  return (
+    <div className="Minter">
+      <button id="walletButton" onClick={connectWalletPressed}>
+        {walletAddress.length > 0 ? (
+          "Connected: " +
+          String(walletAddress).substring(0, 6) +
+          "..." +
+          String(walletAddress).substring(38)
+        ) : (
+          <span>Connect Wallet</span>
+        )}
+      </button>
+
+      <br></br>
+	  <p>
+        Contract address: {contractAddress}
+      </p>
+      <h1 id="title">🧙‍♂️ TheLeadingCrew NFT Minter</h1>
+      <p>
+	  	Enter the amount of token you wish to mint, then press "Mint."
+      </p>
+      <form>
+        <h2>Mint Amount: </h2>
+        <input
+          type="text"
+          placeholder="1 to 3 (for test deployment, change to +/- box)"
+          onChange={(event) => setMintAmount(event.target.value)}
+        />
+      </form>
+      <button id="mintButton" onClick={onMintPressed}>
+        Mint NFT
+      </button>
+      <p id="status" style={{ color: "red" }}>
+        {status}
+      </p>
+    </div>
+  );
+};
+
+export default Minter;
